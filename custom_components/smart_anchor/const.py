d0e5e73d64ee8a0eab6205332ff1d757f1ff00b7
Robifz <smart.boat.innovations@gmail.com>
2024-05-10 23:56:36 +0200

    Create const.py

---
+++ custom_components/smart_anchor/const.py
@@ -0,0 +1,11 @@
+"""Smart Boat Anchor Integration. const.py"""
+
+DOMAIN = "smart_anchor"
+ZONE_ID = "zone.anchor_zone"
+ZONE_NAME = "anchor_zone"
+TRACKER_NAME = "boat_location"
+HELPER_FIELD_ID = "anchor_zone_radius"
+HELPER_FIELD_NAME = "Anchor Zone Radius"
+HELPER_FIELD_ID_ENTITY = "number.anchor_zone_radius"
+
+
